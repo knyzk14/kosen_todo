f10c655d6fc9d867fba0e06e135789e62dec014e
Kyonshi <rt25044s@st.omu.ac.jp>
2026-09-03 18:28:26 +0900

    dashboardのcalendarUIを調整

--- frontend/js/preload.js
+++ frontend/js/preload.js
@@ -1,4 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const html = '<div class="common-background"><img src="/res/img/common-background.png" alt="background" /></div>';
+    const html = '<div class="common-background"></div>';
     document.body.insertAdjacentHTML('afterbegin', html);
 });

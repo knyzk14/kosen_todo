496872d4875db87cbea559bb0a70d83535a61341
Kyonshi <rt25044s@st.omu.ac.jp>
2026-09-22 00:11:37 +0900

    API_VERSIONを修正

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,7 @@ from database import engine
 import models
 from routers import calendars, events, tags, todos, websocket, freebusy
 
-API_VERSION = "1.8.1"
+API_VERSION = "1.9.1"
 
 # Cloudflareを経由した際の実IPを取得
 def get_real_ip(request: Request):
